6338876a8ebd983ba9cda2812c3a106547ea55f2
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 11:30:54 +0000

    added project variable in tfvars and amended iam binding to reflect that its a custom role within a project

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,7 +1,7 @@
 
 resource "google_project_iam_binding" "project" {
   project = "vulnerability-analytics"
-  role    = "roles/custom_va_platform_engineer"
+  role    = "projects/var.project/roles/custom_va_platform_engineer"
 
   members = [
     "user:benmorton8c@example.com",
