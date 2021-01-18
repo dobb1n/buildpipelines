7cdc03a5b3d69123a13bff6fa18d34142edb1e0d
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 12:03:14 +0000

    variables

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,6 +1,6 @@
 
 resource "google_project_iam_member" "project" {
-  project = "vulnerability-analytics"
-  role    = "projects/${var.project}/roles/${google_project_iam_custom_role.custom_role_va_platform_engineer_project_level.custom_va_platform_engineer}"
+  project = "${var.project}"
+  role    = "projects/${var.project}/roles/custom_va_platform_engineer"
   member = "user:benmorton8c@gmail.com"
 }
