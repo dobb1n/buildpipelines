95ee6fdf11c811e3419f797fd7382f90d9e8a4f9
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 11:42:34 +0000

    spurious parenthesis

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -3,5 +3,4 @@ resource "google_project_iam_member" "project" {
   project = "vulnerability-analytics"
   role    = "projects/{project_id}/roles/custom_va_platform_engineer"
   member = "user:benmorton8c@example.com"
-  ]
 }
