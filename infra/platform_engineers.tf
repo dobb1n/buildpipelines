abc349b92d2b433b73cfcf1d61fca5f163c50292
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 11:49:58 +0000

    custom roles need explicit definition in the role field

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,6 +1,6 @@
 
 resource "google_project_iam_member" "project" {
   project = "vulnerability-analytics"
-  role    = "roles/custom_va_platform_engineer"
+  role    = "projects/vulnerability-analytics/roles/custom_va_platform_engineer"
   member = "user:benmorton8c@example.com"
 }
