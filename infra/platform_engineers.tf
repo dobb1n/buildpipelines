16fc3caca28200ab035245568521f150ae530c11
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 11:44:42 +0000

    amended role in iam member to be succinct

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,6 +1,6 @@
 
 resource "google_project_iam_member" "project" {
   project = "vulnerability-analytics"
-  role    = "projects/{project_id}/roles/custom_va_platform_engineer"
+  role    = "roles/custom_va_platform_engineer"
   member = "user:benmorton8c@example.com"
 }
