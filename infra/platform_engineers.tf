90994c01323e54c7b6b67bdf81bb4e42a3a4697e
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 11:32:52 +0000

    amended binding to reflecct projects/{project_id}/roles/{role}

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,7 +1,7 @@
 
 resource "google_project_iam_binding" "project" {
   project = "vulnerability-analytics"
-  role    = "projects/var.project/roles/custom_va_platform_engineer"
+  role    = "projects/{project_id}/roles/custom_va_platform_engineer"
 
   members = [
     "user:benmorton8c@example.com",
