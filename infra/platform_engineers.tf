ab8e3a0d32e7e840d8ea9066165ae8e87b7e8446
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 15:00:58 +0000

    taking out the adding thing to group bit

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,6 +1,6 @@
 
-resource "google_project_iam_member" "project" {
-  project = "vulnerability-analytics"
-  role    = "projects/vulnerability-analytics/roles/custom_va_platform_engineer"
-  member = "user:dobbinwondermule@gmail.com"
-}
+# resource "google_project_iam_member" "project" {
+#   project = "vulnerability-analytics"
+#   role    = "projects/vulnerability-analytics/roles/custom_va_platform_engineer"
+#   member = "user:dobbinwondermule@gmail.com"
+# }
