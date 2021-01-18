0edaa00a847766ad0f679b0d01ee36bb819a35ff
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 12:07:34 +0000

    removed tfvars

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,6 +1,6 @@
 
 resource "google_project_iam_member" "project" {
-  project = "${var.project}"
-  role    = "projects/${var.project}/roles/custom_va_platform_engineer"
+  project = "vulnerability-analytics"
+  role    = "projects/vulnerability-analytics/roles/custom_va_platform_engineer"
   member = "user:benmorton8c@gmail.com"
 }
