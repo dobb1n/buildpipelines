60c6139166b07f6e6f004bd92c18d8258b0a0e77
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 10:06:00 +0000

    removed commas

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -2,13 +2,13 @@
 
 data "google_iam_policy" "admin" {
   binding {
-    role = "roles/notebooks.instances.start",
-    role = "roles/notebooks.instances.stop",
-    role = "roles/compute.networkViewer",
-    role = "roles/compute.viewer",
+    role = "roles/notebooks.instances.start"
+    role = "roles/notebooks.instances.stop"
+    role = "roles/compute.networkViewer"
+    role = "roles/compute.viewer"
     role = "compute.instances.setLabels"
     members = [
-      "user:benmorton8c@example.com",
+      "user:benmorton8c@example.com"
     ]
   }
 }
