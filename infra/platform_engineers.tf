28fd7671bc46fbd764969f62b89a9c010fdeb6f2
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 10:53:50 +0000

    changed to project binding

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,14 +1,9 @@
-resource "google_project_iam_policy" "project" {
-  project     = "vulnerability-analytics"
-  policy_data = data.google_iam_policy.admin.policy_data
-}
 
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/custom_va_platform_engineer"
+resource "google_project_iam_binding" "project" {
+  project = "vulnerability-analytics"
+  role    = "roles/custom_va_platform_engineer"
 
-    members = [
-      "user:benmorton8c@example.com",
-    ]
-  }
+  members = [
+    "user:benmorton8c@example.com",
+  ]
 }
