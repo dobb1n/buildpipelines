80cd0ea99ec3fcc4810227e580b03b74a39e09af
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 14:59:17 +0000

    trying member and dobbinwondermule

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,8 +1,6 @@
 
-resource "google_project_iam_binding" "project" {
+resource "google_project_iam_member" "project" {
   project = "vulnerability-analytics"
   role    = "projects/vulnerability-analytics/roles/custom_va_platform_engineer"
-  members = [
-    "user:benmorton8c@gmail.com"
-  ]
+  member = "user:dobbinwondermule@gmail.com"
 }
