b175ee59a8012e867d1efaa28e0b4fea8c4a71e4
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 12:00:48 +0000

    following git issue

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -1,6 +1,6 @@
 
 resource "google_project_iam_member" "project" {
   project = "vulnerability-analytics"
-  role    = "projects/vulnerability-analytics/roles/custom_va_platform_engineer"
+  role    = "projects/${var.project}/roles/${google_project_iam_custom_role.custom_role_va_platform_engineer_project_level.custom_va_platform_engineer}"
   member = "user:benmorton8c@gmail.com"
 }
