d132fca191025d5f4a09a8e33a83b645aa448933
gva-bmorton <ben.morton@lloydsbanking.com>
2021-01-18 10:03:54 +0000

    updated to just reflect the roles i wanted

--- infra/platform_engineers.tf
+++ infra/platform_engineers.tf
@@ -2,7 +2,11 @@
 
 data "google_iam_policy" "admin" {
   binding {
-    role = "roles/viewer"
+    role = "roles/notebooks.instances.start",
+    role = "roles/notebooks.instances.stop",
+    role = "roles/compute.networkViewer",
+    role = "roles/compute.viewer",
+    role = "compute.instances.setLabels"
     members = [
       "user:benmorton8c@example.com",
     ]
